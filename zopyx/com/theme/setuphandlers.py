84ffae7992fc5cf1cef5fd8febcf46a1dd0bad47
zopyx <yet@gmx.de>
2012-11-14 14:45:24 +0100

    updated

--- zopyx/com/theme/setuphandlers.py
+++ zopyx/com/theme/setuphandlers.py
@@ -5,7 +5,7 @@ def setupVarious(context):
     # flag to check that we actually meant for this import step to be run.
     # The file is found in profiles/default.
 
-    if context.readDataFile('zopyx.theme_various.txt') is None:
+    if context.readDataFile('zopyx.com.theme_various.txt') is None:
         return
 
     # Add additional setup code here
