5e1a8bc2ea4f3f67f942417b2ae2657cff6b4a98
zopyx <yet@gmx.de>
2012-11-15 11:01:36 +0100

    updated

--- zopyx/com/theme/__init__.py
+++ zopyx/com/theme/__init__.py
@@ -3,3 +3,10 @@
 
 def initialize(context):
     """Initializer called when used as a Zope 2 product."""
+
+from Products.CMFCore.DirectoryView import registerFileExtension
+from Products.CMFCore.FSFile import FSFile
+
+registerFileExtension('svg', FSFile)
+registerFileExtension('svg', FSFile)
+registerFileExtension('ttf', FSFile)
